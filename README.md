48b22298ae3675540b4e2798f5a62eb5088b7dc1
TimothyMaraj <timxmaraj@gmail.com>
2024-04-23 00:40:10 -0500

    plans and scaffolding

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ollamCCLI
+OllamaCCLI is a Custom Command Line Interface built for fun. It serves as a practical custom command line tool to work with the Ollama application using the Llama3 model. 
+
+
+# Future Plans
+- implement model switching/ selection
+- list useable models 
--- llama.py
+++ llama.py
@@ -14,9 +14,30 @@ def funciton generate
     - route
 
  - handle the request 
+ -> get request with ceratin body info like 
+ '{
+  "model": "llama2",
+  "prompt": "What color is the sky at different times of the day? Respond using JSON",
+  "format": "json",
+  "stream": false
+}'
 
+- note model here will be llama3 
+- prompt is what changes as userinput
+- format should always be json
+- stream false will respond one json response but may be slower (in most cases on test) , stream: true is faster but requires more parsing
+
+- def use the requests library and json library to work with the response 
 
 """
+
+
+# a single response handler
+def response(): 
+    pass
+
+def streamResponse(): 
+    pass
 
 def main():
     pass
