5240f7908d47f4c860d8b4ab64ee36077919ec89
TimothyMaraj <timxmaraj@gmail.com>
2024-04-22 20:47:26 -0500

    first commit

---
+++ llama.py
@@ -0,0 +1,25 @@
+import json
+import requests
+import argparse
+
+PORT = 11434
+MODEL = 'llama3'
+
+"""
+llamaccli --generate "<prompt>"
+
+def funciton generate
+ - build the request 
+    - method
+    - route
+
+ - handle the request 
+
+
+"""
+
+def main():
+    pass
+
+if __name__ == '__main__': 
+    main()
