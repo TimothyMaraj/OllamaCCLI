ec7ed3cd013f31b7bda0843fa25ea7ae12f0b5f2
TimothyMaraj <timxmaraj@gmail.com>
2024-04-23 21:53:16 -0500

    added plans for diff functions, took an oop route filled out some functions like changeModel

--- llama.py
+++ llama.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python3
 import json
-import requests
+import requests as request
 import argparse
+from dotenv import load_dotenv, set_key, dotenv_values
+import os
 
-PORT = 11434
-MODEL = 'llama3'
+load_dotenv()
+
 
 """
 llamaccli --generate "<prompt>"
@@ -29,18 +32,137 @@ def funciton generate
 
 - def use the requests library and json library to work with the response 
 
+
+
+
+
+
+
+
+// command line tool name will be names: 
+
+
+
+
+
+
+
+
 """
 
 
-# a single response handler
-def response(): 
-    pass
+# Attempt at OOPing this 
 
-def streamResponse(): 
-    pass
+class user(): 
+    def __init__(self,inputPrompt=""):
+        user.prompt=inputPrompt
+        user.model= os.getenv("MODEL")
+        print(user.model)
+       ## Load existing environment variables from the .env file
+       #env_vars = dotenv_values(".env")
+
+       ## Change the value of the "MODEL" variable to "Gemma"
+       #env_vars["MODEL"] = "Gemma"
+
+       ## Update the .env file with the new value
+       #set_key(".env", "MODEL", env_vars["MODEL"])
+       #os.environ["MODEL"] = env_vars["MODEL"]
+
+       #user.model= os.getenv("MODEL")
+       #print(user.model)
+
+
+    def getModelData(self):
+        with open('models.json','r') as file: 
+            models = json.load(file)
+        return models["models"]
+
+    def changeModel(self,newModel):
+        """
+        - code to list models
+        - code to change model
+        - continously running or change some variable in the .env
+        # Access environment variables
+        variable_value = os.getenv("VARIABLE_NAME")
+
+        # Modify environment variables
+        os.environ["VARIABLE_NAME"] = "new_value"
+        """
+        models = self.getModelData()
+        print(models)
+        if newModel not in models: 
+            print(models.items())
+            print("\033[31mModel Not Found\033[0m")
+            return None
+        if newModel == os.environ["MODEL"]: 
+            print("\033[31mMODEL ALREADY IN USE\033[0m")
+            return None
+            
+
+    def listModels(self):
+        """
+        - should list models avialable from a list? 
+        - put in env varaibles for now ? 
+        - using the api to get this list && updating the json wit it 
+
+        API: 
+            GET /api/tags
+
+            json response like: 
+            {
+            "models": [
+                {
+                "name": "codellama:13b",
+                "modified_at": "2023-11-04T14:56:49.277302595-07:00",
+                "size": 7365960935,
+                "digest": "9f438cb9cd581fc025612d27f7c1a6669ff83a8bb0ed86c94fcf4c5440555697",
+                "details": {
+                    "format": "gguf",
+                    "family": "llama",
+                    "families": null,
+                    "parameter_size": "13B",
+                    "quantization_level": "Q4_0"
+                }
+                },
+                    {
+                "name": "llama2:latest",
+                "modified_at": "2023-12-07T09:32:18.757212583-08:00",
+                "size": 3825819519,
+                "digest": "fe938a131f40e6f6d40083c9f0f430a515233eb2edaa6d72eb85c50d64f2300e",
+                "details": {
+                    "format": "gguf",
+                    "family": "llama",
+                    "families": null,
+                    "parameter_size": "7B",
+                    "quantization_level": "Q4_0"
+                    }
+                    }
+                ]
+            }
+        """
+        pass
+    def updateModelList(self): 
+        """
+        - updates the json if a new model is installed? 
+        - for now user input on models
+        """
+        pass
+    def requestBuilder(self): 
+        pass
+    def response(self,prompt=""): 
+        request.post()
+    def streamResponse(self): 
+        pass
+
+# a single response handler
+
+
 
 def main():
-    pass
+
+    olamCCLi=user("sup")
+    olamCCLi.changeModel("llama3")
+    
 
 if __name__ == '__main__': 
     main()
